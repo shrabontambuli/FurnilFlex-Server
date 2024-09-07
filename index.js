57150270f14ff17fc4150bef565657075329dc14
Shrabon Tambuli <shrabonsa@gmail.com>
2024-09-07 20:17:11 +0600

    changes

--- index.js
+++ index.js
@@ -11,11 +11,9 @@ app.use(cors());
 app.use(express.json());
 // app.use(express.urlencoded());
 
-// FurnilFlex
-// VqeyCRBZuRoG7nxp
 
 
-const uri = "mongodb+srv://FurnilFlex:VqeyCRBZuRoG7nxp@cluster0.ezafyme.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ezafyme.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const userCollection = client.db('FurnilFlex').collection('users');
         const productCollection = client.db('FurnilFlex').collection('products');
@@ -106,17 +104,14 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
 run().catch(console.dir);
-
-
-
 
 
 app.get('/', (req, res) => {
